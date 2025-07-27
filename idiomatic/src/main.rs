b7aa6c6be465103c1dde354cba34b63a8b50c992
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-27 19:32:04 +0530

    idiomatic - on resolving clippy issues

--- idiomatic/src/main.rs
+++ idiomatic/src/main.rs
@@ -8,22 +8,23 @@
 
 // Challenge: Clippy doesn't find *everything*. What else would you change to make this code better?
 
-const pi: f32 = 3.14159265358979323846;
+use std::f64::consts::PI;
+
 fn count_to_5() -> i32 {
-    let mut foo = 0;
+    let mut number = 0;
     loop {
-        if foo > pi as i32 {
-            if foo > 5 {
-                break;
-            }
+        if number > PI as i32 && number > 5 {
+            break;
         }
-        foo = foo + 1;
+        number += 1;
     }
-    return 5;
+    5
 }
+
 fn main() {
     println!("I can count to {}", count_to_5());
 }
+
 #[cfg(test)]
 mod test {
     use super::*;
