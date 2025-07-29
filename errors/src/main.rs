01fe65f1aa2042f1c829d45bf2fd2ae0f85d9150
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2025-07-29 22:42:51 +0530

    errors - challenge

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -28,7 +28,7 @@ fn play_time(dolphin: &Dolphin) -> Result<Vec<String>> {
     Ok(responses)
 }
 
-fn main() {
+fn main() -> Result<()>{
     let dolphins = vec![
         Dolphin {
             name: "Augustinius".into(),
@@ -59,14 +59,20 @@ fn main() {
         // returns an Err variant the first time it is called, the try operator will return it from
         // main(), which will end the program at the first error. anyhow's Result will take care of
         // formatting the error output for us.
-        match play_time(dolphin) {
-            Ok(responses) => {
-                println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
-                for response in responses {
-                    println!("  {}", response);
-                }
-            },
-            Err(e) => println!("{} can't perform today: {}", dolphin.name, e.to_string()),
+        // match play_time(dolphin) {
+        //     Ok(responses) => {
+        //         println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
+        //         for response in responses {
+        //             println!("  {}", response);
+        //         }
+        //     },
+        //     Err(e) => println!("{} can't perform today: {}", dolphin.name, e.to_string()),
+        // }
+        let responses = play_time(dolphin)?;
+        println!("{} did a FABULOUS PERFORMANCE!", dolphin.name);
+        for response in responses {
+            println!("  {}", response);
         }
     }
+    Ok(())
 }
